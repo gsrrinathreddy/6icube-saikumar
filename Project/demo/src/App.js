2abf3156969121143e8aeedbc5f267eedf461468
saikumar <sai18ek5a0307@gmail.com>
2023-04-27 18:36:58 +0530

    Adding Project

--- Project/demo/src/App.js
+++ Project/demo/src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Navbar from './Components/Navbar';
-
+import Pricing from './Components/Pages/Pricing';
+import Products from './Components/Pages/Products';
+import Blog from './Components/Pages/Blog';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path="Products" element={<Products/>}/>
+        <Route path="Blog" element={<Blog/>}/>
+        <Route path="Pricing" element={<Pricing/>}/>
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- Project/demo/src/Components/Navbar/index.js
+++ Project/demo/src/Components/Navbar/index.js
@@ -12,6 +12,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import { Link } from 'react-router-dom';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -115,6 +116,7 @@ function Navbar() {
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
+                <Link to={page} style={{textDecoration:"none"}}>
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
@@ -122,6 +124,7 @@ function Navbar() {
               >
                 {page}
               </Button>
+              </Link>
             ))}
           </Box>
 
